19a7b24bc935c412ec7bdb500737e8b3cae50b0c
Isys Naila <nailaisys2017@gmail.com>
2025-06-04 20:21:11 -0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -2,7 +2,7 @@
 ALUNA: ISYS NAILA SANTOS SILVA      RA:16424
 
 
-1-Crie uma FUNCTION de nome “contagem_conta” que retorne o número total de contas que determinado cliente possui, a partir do código do cliente (parâmetro da função).  
+--Crie uma FUNCTION de nome “contagem_conta” que retorne o número total de contas que determinado cliente possui, a partir do código do cliente (parâmetro da função).  
 
 
 CREATE OR REPLACE FUNCTION contagem_conta (codcli CHAR(4))
@@ -17,7 +17,7 @@ end;
 $$
 
 
-a)Selecione o nome, a cidade, o estado e o número total de contas bancárias dos clientes cadastrados.
+--Selecione o nome, a cidade, o estado e o número total de contas bancárias dos clientes cadastrados.
 
 
 SELECT cliente.nomeCliente, cliente.cidadeCliente, cliente.estadoCliente, contagem_conta(cliente.codCliente)
@@ -25,7 +25,7 @@ AS total_contas
 FROM cliente;
 
 
-2-Crie uma FUNCTION de nome “total_contas” que retorne o número total de contas que determinada Agência possui, a partir do código da agência (parâmetro da função).  
+--Crie uma FUNCTION de nome “total_contas” que retorne o número total de contas que determinada Agência possui, a partir do código da agência (parâmetro da função).  
 
 
 CREATE OR REPLACE FUNCTION total_contas (cod_agencia CHAR(5))
@@ -40,7 +40,7 @@ end;
 $$
 
 
-a) Selecione o nome e a cidade das Agências cujo total de contas cadastradas seja maior ou igual a 2.
+--Selecione o nome e a cidade das Agências cujo total de contas cadastradas seja maior ou igual a 2.
 
 
 SELECT agencia.nomeAgencia, agencia.cidadeAgencia
@@ -48,7 +48,7 @@ FROM agencia
 WHERE total_contas(agencia.codAgencia)>=2;
 
 
-3-Crie uma FUNCTION de nome “saldo_total_cliente” que retorne o somatório dos saldos de todas as contas que cada cliente possui, a partir do código do cliente (parâmetro da função).
+--Crie uma FUNCTION de nome “saldo_total_cliente” que retorne o somatório dos saldos de todas as contas que cada cliente possui, a partir do código do cliente (parâmetro da função).
 
 
 CREATE OR REPLACE FUNCTION saldo_total_cliente (codcli CHAR(4))
@@ -63,14 +63,14 @@ end;
 $$
 
 
-a) Selecione o nome, a cidade, o estado e o saldo total de todos os clientes.
+--Selecione o nome, a cidade, o estado e o saldo total de todos os clientes.
 
 
 SELECT cliente.nomeCliente, cliente.estadoCliente,cliente.cidadeCliente, saldo_total_cliente(cliente.codCliente) AS saldo_total
 FROM cliente
 
 
-b)Selecione o nome, a cidade, o estado e o saldo total do(a) cliente com código ‘0111’.
+--Selecione o nome, a cidade, o estado e o saldo total do(a) cliente com código ‘0111’.
 
 
 SELECT cliente.nomeCliente, cliente.estadoCliente,cliente.cidadeCliente, saldo_total_cliente(cliente.codCliente) AS saldo_total
@@ -78,7 +78,7 @@ FROM cliente
 WHERE codCliente='0111'
 
 
-c)Selecione o nome, a cidade, o estado e o saldo total dos clientes cujo saldo total seja maior que cem mil (100000.00).
+--Selecione o nome, a cidade, o estado e o saldo total dos clientes cujo saldo total seja maior que cem mil (100000.00).
 
 
 SELECT cliente.nomeCliente, cliente.estadoCliente,cliente.cidadeCliente
@@ -86,7 +86,7 @@ FROM cliente
 WHERE saldo_total_cliente(cliente.codCliente)>100000
 
 
-4- Crie uma FUNCTION de nome “maximo_saldo” que retorne o maior valor de saldo em conta existente em cada agência, a partir do código da agência (parâmetro da função)
+--Crie uma FUNCTION de nome “maximo_saldo” que retorne o maior valor de saldo em conta existente em cada agência, a partir do código da agência (parâmetro da função)
 
 
 CREATE FUNCTION maximo_saldo (cod_agencia CHAR(5))
@@ -101,7 +101,7 @@ end;
 $$
 
 
-a)Selecione o nome e a cidade da agência bem como o nome do cliente que possui o maior saldo e o valor do maior saldo, agrupados por agência e ordenados pelo valor do saldo, do maior para o menor. 
+--Selecione o nome e a cidade da agência bem como o nome do cliente que possui o maior saldo e o valor do maior saldo, agrupados por agência e ordenados pelo valor do saldo, do maior para o menor. 
 
 
 SELECT agencia.nomeAgencia, agencia.cidadeAgencia, cliente.nomeCliente,conta.saldo
@@ -114,7 +114,7 @@ WHERE conta.saldo=maximo_saldo(agencia.codAgencia)
 ORDER BY conta.saldo DESC
 
 
-5-Crie uma FUNCTION de nome “minimo_saldo” que retorne o menor valor de saldo em conta existente em cada agência, a partir do código da agência (parâmetro da função).
+--Crie uma FUNCTION de nome “minimo_saldo” que retorne o menor valor de saldo em conta existente em cada agência, a partir do código da agência (parâmetro da função).
 
 
 CREATE FUNCTION minimo_saldo (cod_agencia CHAR(5))
@@ -128,7 +128,7 @@ RETURN menor_saldo;
 end;
 $$
          
-a)Selecione o nome e a cidade da agência bem como o nome do cliente que possui o menor saldo e o valor do menor saldo, agrupados por agência e ordenados pelo valor do saldo, do menor para o maior.
+--Selecione o nome e a cidade da agência bem como o nome do cliente que possui o menor saldo e o valor do menor saldo, agrupados por agência e ordenados pelo valor do saldo, do menor para o maior.
 
 
 SELECT agencia.nomeAgencia, agencia.cidadeAgencia, cliente.nomeCliente,conta.saldo
